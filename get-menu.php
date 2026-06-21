9f7cc450494a35af15aff2aa86fe0f3b5d004e9e
pabloosss <notingss@gmail.com>
2026-06-21 21:50:59 +0200

    Read public menu from PHP store first

--- get-menu.php
+++ get-menu.php
@@ -1,10 +1,20 @@
 <?php
-require __DIR__ . '/app-config.php';
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-store');
-if (!file_exists(MENU_FILE)) {
-  http_response_code(404);
-  echo json_encode(['products' => [], 'ingredients' => []], JSON_UNESCAPED_UNICODE);
-  exit;
+
+$menuFile = __DIR__ . '/menu-data.php';
+if (file_exists($menuFile)) {
+    $menu = include $menuFile;
+    if (is_array($menu)) {
+        echo json_encode($menu, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit;
+    }
 }
-echo file_get_contents(MENU_FILE);
+
+require __DIR__ . '/app-config.php';
+if (file_exists(MENU_FILE)) {
+    echo file_get_contents(MENU_FILE);
+    exit;
+}
+
+echo json_encode(array('products' => array(), 'ingredients' => array()), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
